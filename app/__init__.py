9812d155ba7e6eaa2adfae3ee1e46d89e184b3c2
Mycroft <mycroftlee@gmail.com>
2025-12-03 10:40:20 -0500

    edited init

--- app/__init__.py
+++ app/__init__.py
@@ -15,6 +15,9 @@ cursor = db.cursor()
 #--------------------------------------------------------------------------
 #CODE
 
+#QoL db commands
+def accessitem(tablename, columnname): #accesses "columnname" from a chosen "tablename" in db
+    cursor.execute(f"Select"+columnname+"from"+tablename)
 
-def standardize():
+def accessitem_s(tablename, columnname, itemname): #accesses a single item from a given column
     
