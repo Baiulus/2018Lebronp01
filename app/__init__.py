debeb5cc28bbdd6fbe2ebb0716403d22b49b1090
Mycroft Lee <mycroftlee@gmail.com>
2025-12-03 19:50:44 -0500

    updated init

--- app/__init__.py
+++ app/__init__.py
@@ -17,7 +17,21 @@ cursor = db.cursor()
 
 #QoL db commands
 def accessitem(tablename, columnname): #accesses "columnname" from a chosen "tablename" in db
-    cursor.execute(f"Select"+columnname+"from"+tablename)
+    cursor.execute(f"Select"+columnname+"from"+tablename) #input: select [__COLUMNNAME__] from [__TABLENAME__]
 
 def accessitem_s(tablename, columnname, itemname): #accesses a single item from a given column
+    cursor.execute(f"Select * from"+tablename+"where"+columnname+"= '"+ itemname + "'") #input: select * from [__TABLENAME__] where [__COLUMNNAME__] = '[__ITEMNAME__]'
     
+def accessitem_m(tablename, columnname, itemarray): #accesses any item from the itemarray in a given column
+    
+    for item in itemarray: #adds [']s to each item in itemarray to make it readable by sqlite
+        item = "'"+item+"'" 
+    
+    items = ', '.join(map(str, itemarray)) #removes the brackets from itemarray to make items readable by sqlite
+    
+    cursor.execute(f"Select * from" + tablename + "where" + columnname + "in (" + items + ")") #input: select * from [__TABLENAME__] where [__COLUMNNAME__] in [__ITEMS__]
+    
+    
+#--------------------------------------------------------------------------
+#TESTS
+
